90b893be9fce80fd95f143882c92e28d4f3b2759
Francisco Castro <fcr@adinet.com.uy>
2025-04-06 04:45:03 -0300

    Update calculator with a bazillion new commands

--- test_calc.c
+++ test_calc.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -18,10 +19,11 @@ struct {
     int size;
 } stack = {.size=0};
 
-void push(int n) {
+int push(int n) {
     stack.data[stack.size++] = n;
     if (stack.size > MAX_STACK_SIZE)
         error(1, "stack overflow\n");
+    return n;
 }
 int pop() {
     if (stack.size <= 0)
@@ -29,6 +31,85 @@ int pop() {
 
     return stack.data[--stack.size];
 }
+
+uint32_t hash(const char *text) {
+    if (!text) return 0;
+    uint32_t res = 0x811c9dc5;
+    while (*text) res = (res ^ *(text++)) * 0x01000193;
+    return res;
+}
+
+typedef void(command_fn_t)();
+
+#define COMMANDS_TABLE_SIZE 32
+#define MAX_COMMAND_COLLISIONS 3
+struct {
+    const char *name;
+    command_fn_t *fn;
+} commands[COMMANDS_TABLE_SIZE] = {[0 ... (COMMANDS_TABLE_SIZE-1)] = 0};
+
+void register_command(const char *name, command_fn_t *fn) {
+    uint32_t bucket = hash(name) % COMMANDS_TABLE_SIZE;
+    for (int c = 0; c < MAX_COMMAND_COLLISIONS; c++) {
+        if (!commands[bucket].name) {
+            commands[bucket].name = name;
+            commands[bucket].fn = fn;
+            return;
+        }
+        bucket = (bucket + 1) % COMMANDS_TABLE_SIZE;
+    }
+    error(1,
+        "max collisions found for command «%s», "
+        "increase COMMANDS_TABLE_SIZE (to next power of 2)\n"
+        "or MAX_COMMAND_COLLISIONS (if usage < 50 percent)\n",
+        name);
+}
+
+command_fn_t * find_command(const char *name) {
+    uint32_t bucket = hash(name) % COMMANDS_TABLE_SIZE;
+    for (int c = 0; c < MAX_COMMAND_COLLISIONS; c++) {
+        if (!commands[bucket].name) return NULL;
+        if (!strcmp(name, commands[bucket].name)) return commands[bucket].fn;
+        bucket = (bucket + 1) % COMMANDS_TABLE_SIZE;
+    }
+    return NULL;
+}
+
+void command_add() { push(pop() + pop()); }
+void command_and() { push(pop() & pop()); }
+void command_bitshift() {
+    int shift = pop();
+    unsigned int num = pop();
+    push(shift >= 0 ? (num<<shift) : (num>>-shift));
+}
+void command_copy() {
+    int index = pop();
+    if (index < 0) error(1, "negative index\n");
+    if (index > stack.size) error(1, "index underflow\n");
+    for (int i = 0; i < index; i++)
+        push(stack.data[stack.size - index]);
+}
+void command_count() { push(stack.size); }
+void command_dup() { push(push(pop())); }
+void command_exch() {
+    int x = pop();
+    int y = pop();
+    push(x);
+    push(y);
+}
+void command_idiv() { int q = pop(); push(pop() / q); }
+void command_index() {
+    int index = pop();
+    if (index < 0) error(1, "negative index\n");
+    if (index >= stack.size) error(1, "index underflow\n");
+    push(stack.data[stack.size - index - 1]);
+}
+void command_mod() { int q = pop(); push(pop() % q); }
+void command_mul() { push(pop() * pop()); }
+void command_neg() { push(-pop()); }
+void command_sub() { int x = pop(); push(pop() - x); }
+void command_weight() { push(__builtin_popcount(pop())); }
+void command_xor() { push(pop() ^ pop()); }
 
 int main(int argc, char * argv[]) {
     //printf("argc=%d\n", argc);
@@ -40,51 +121,58 @@ int main(int argc, char * argv[]) {
             "it's going to be pushed in the stack, otherwise operations are:\n"
             "\n"
             "int1 int2 <add>   int:       computes int1 + int2\n"
+            "int1 int2 <and>   int:       computes bitwise int1 AND int2\n"
+            "int1 int2 <bitshift> int:    logic shifts int1 by int2 bits (left pos, right neg)\n"
+            "  v1…vn n <copy> v1…vn v1…vn:  copies the top n values\n"
+            "  ⊢ v1…vn <count» v1…vn n:   counts the number of elements in the stack\n"
             "        v <dup>   v:         duplicates top value\n"
             "    v1 v2 <exch>  v2 v1:     swaps top two values\n"
             "int1 int2 <idiv>  int:       computes signed division int1 / int2\n"
             "  vn…v0 n <index> vn…v0 vn:  pushes the n-th value\n"
             "int1 int2 <mod>   int:       computes the remainder of dividing int1 by int2\n"
             "int1 int2 <mul>   int:       computes int1 * int2\n"
-            "int1 int2 <sub>   int:       computes int1 - int2\n",
+            "     int1 <neg>   int:       computes -int1\n"
+            "int1 int2 <sub>   int:       computes int1 - int2\n"
+            "     int1 <weight> int:      computes the number of 1 bits in int1\n"
+            "int1 int2 <xor>   int:       computes bitwise int1 XOR int2\n",
             *argv
         );
     }
+    register_command("add", command_add);
+    register_command("and", command_and);
+    register_command("bitshift", command_bitshift);
+    register_command("copy", command_copy);
+    register_command("count", command_count);
+    register_command("dup", command_dup);
+    register_command("exch", command_exch);
+    register_command("idiv", command_idiv);
+    register_command("index", command_index);
+    register_command("mod", command_mod);
+    register_command("mul", command_mul);
+    register_command("neg", command_neg);
+    register_command("sub", command_sub);
+    register_command("weight", command_weight);
+    register_command("xor", command_xor);
+
     for (int i = 1; i < argc; i++) {
         char * endptr;
         char * arg = argv[i];
         int n = strtol(arg, &endptr, 0);
+        command_fn_t * fn;
         if (*arg && !*endptr) {
             push(n);
-        } else if (!strcmp(arg, "add")) {
-            push(pop() + pop());
-        } else if (!strcmp(arg, "dup")) {
-            int x = pop();
-            push(x);
-            push(x);
-        } else if (!strcmp(arg, "exch")) {
-            int x = pop();
-            int y = pop();
-            push(x);
-            push(y);
-        } else if (!strcmp(arg, "idiv")) {
-            int q = pop();
-            push(pop() / q);
-        } else if (!strcmp(arg, "index")) {
-            int index = pop();
-            if (index < 0) error(1, "negative index\n");
-            if (index >= stack.size) error(1, "index underflow\n");
-            push(stack.data[stack.size - index - 1]);
-        } else if (!strcmp(arg, "mod")) {
-            int q = pop();
-            push(pop() % q);
-        } else if (!strcmp(arg, "mul")) {
-            push(pop() * pop());
-        } else if (!strcmp(arg, "sub")) {
-            int sub2 = pop();
-            push(pop() - sub2);
+        } else if ((fn = find_command(arg))) {
+            fn();
         } else {
-            fprintf(stderr, "unsupported command «%s»\n", argv[i]);
+            fprintf(stderr, "unsupported command «%s» hash=%x, supported commands:\n", arg, hash(arg));
+            int count = 0;
+            for (int i = 0; i < COMMANDS_TABLE_SIZE; i++)
+                if (commands[i].name) {
+                    count++;
+                    fprintf(stderr, "  (bucket=%03x, hash=%08x) «%s»\n", i, hash(commands[i].name), commands[i].name);
+                }
+
+            printf("command hash usage %d%%\n", 100*count/COMMANDS_TABLE_SIZE);
             return 1;
         }
     }
